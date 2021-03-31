ee5209eeb0b8f7d57f42e6cc7ece241bea3a9fcc
Rafa-Oli <rafaela.albaniza@gmail.com>
2021-03-31 19:39:33 -0300

    initial

---
+++ mobX1/states/lib/controller.g.dart
@@ -0,0 +1,39 @@
+part of 'controller.dart';
+
+mixin _$Controller on ControllerBase, Store {
+  final _$counterAtom = Atom(name: 'ControllerBase.counter');
+
+  @override
+  int get counter {
+    _$counterAtom.reportRead();
+    return super.counter;
+  }
+
+  @override
+  set counter(int value) {
+    _$counterAtom.reportWrite(value, super.counter, () {
+      super.counter = value;
+    });
+  }
+
+  final _$ControllerBaseActionController =
+      ActionController(name: 'ControllerBase');
+
+  @override
+  dynamic increment() {
+    final _$actionInfo = _$ControllerBaseActionController.startAction(
+        name: 'ControllerBase.increment');
+    try {
+      return super.increment();
+    } finally {
+      _$ControllerBaseActionController.endAction(_$actionInfo);
+    }
+  }
+
+  @override
+  String toString() {
+    return '''
+counter: ${counter}
+    ''';
+  }
+}
